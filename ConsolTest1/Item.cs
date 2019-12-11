e8fc366f1610b1d7e4ae80ae091a05e06111f177
myrikor@gmail.com <myrikor@gmail.com>
2019-12-12 00:29:34 +0600

    List test

---
+++ ConsolTest1/Item.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+namespace ConsolTest1
+{
+    
+    public class Item
+    {
+        public int Value;
+        public Item Next;
+
+        public static void ReverseItem(ref Item list)
+        {
+            Item first;
+            Item rest;
+            if (list == null) { return; }
+            first = list;
+            rest = first.Next;
+            if (rest == null) { return; }
+            ReverseItem(ref rest);
+            first.Next.Next = first;
+            first.Next = null;
+            list = rest;
+        }
+        public static void PrintItem(Item list)
+        {
+            Item tmp = list;
+            while (tmp != null)
+            {
+                Console.WriteLine(tmp.Value);
+                tmp = tmp.Next;
+            }
+        }
+        public static void AddItem(Item l, Item i)
+        {
+            while (l.Next != null)
+            {
+                l = l.Next;
+            }
+            l.Next = i;
+        }
+        public static void PrintItemEven(Item list)
+        {
+            int i = 1;
+            while (list.Next != null)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(list.Value);
+                }
+                i++;
+                list = list.Next;
+                if (i % 2 == 0 && list.Next == null)
+                {
+                    Console.WriteLine(list.Value);
+                }
+            }
+        }
+        public static void PrintItemDenEven(Item list)
+        {
+            int i = 0;
+            while (list.Next != null)
+            {
+                i++;
+                list = list.Next;
+
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(list.Value);
+                }
+            }
+        }
+        public static bool Cycled(Item l)
+        {
+            bool cycl = false;
+            Item tmp, tmp1;
+            tmp = tmp1 = l;
+            l = l.Next;
+            while (tmp != null)
+            {
+                while (l != null)
+                {
+                    if (tmp == l)
+                    {
+                        cycl = true;
+                        return cycl;
+                    }
+                    else
+                    {
+                        l = l.Next;
+                    }
+                    if (tmp1 == l) { break; }
+                }
+                tmp = tmp.Next;
+            }
+            return cycl;
+        }
+        public static Item CreationItemList(int n)
+        {
+            Item List = new Item() { Value = 0 };
+            Item tmp = List;
+
+            for (int i = 1; i < n; i++)
+            {
+                tmp.Next = new Item() { Value = i };
+                tmp = tmp.Next;
+            }
+            return List;
+        }
+    }
+
+}
---
+++ ConsolTest1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using static ConsolTest1.Item;
+
+namespace ConsolTest1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Item testlist = CreationItemList(10);
+
+            //PrintItem(testlist);
+
+            PrintItemEven(testlist);
+
+            Console.WriteLine(Cycled(testlist));
+
+            //ReverseItem(ref testlist);
+
+            //PrintItem(testlist);
+
+            PrintItemDenEven(testlist);
+        }
+
+        
+    }
+}
