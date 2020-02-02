e3b6059f0d2523eb1c95fa79a3b1b840126e3f10
Xigi <myrikor@gmail.com>
2020-02-02 16:44:08 +0600

    Add new Project

---
+++ Getnumbers/Getnumber.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Getnumbers
+{
+    public class Getnumber
+    {
+        public static int range;
+        public static int[] number;
+        public static int position = 0;
+        public Getnumber(int n)
+        {
+            range = n;
+        }
+
+        public static int[] Arraynumber()
+        {
+            
+            number = new int[range];
+            for (int i=0; i< range; i++)
+            {
+                number[i] = i; 
+            }
+            return number;
+        }
+
+        public static int[] Take (int n)
+        {
+            number = Arraynumber();
+            int[] result = new int[n];
+            for (int i =0; i<n; i++)
+            {
+                result[i] = number[position+i];
+            }
+            position+=n;
+            return result;
+            
+        }
+       
+
+
+        //public static int[] NumberArray(int n)
+        //{
+        //    number = new int[n];
+        //    for(int i =0; i < n; i++)
+        //    {
+        //        number[i] = i;
+        //    }
+        //    return number;
+
+    }
+}
---
+++ Getnumbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Getnumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = new Getnumber(30);
+            int[] test = Getnumber.Take(10);
+
+            foreach (int element in test)
+            {
+                Console.Write(element+" ");
+            }
+
+            Console.WriteLine();
+
+            int[] test1 = Getnumber.Take(10);
+
+            foreach (int element in test1)
+            {
+                Console.Write(element+" ");
+            }
+
+            Console.WriteLine();
+
+            int[] test2 = Getnumber.Take(10);
+
+            foreach (int element in test2)
+            {
+                Console.Write(element + " ");
+            }
+        }
+    }
+}
