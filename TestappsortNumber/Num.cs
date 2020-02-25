b1eabb70d4290cc741890c94636f6f531315385d
Xigi <myrikor@gmail.com>
2020-02-25 16:51:27 +0600

    IEnurabl test projeckt

---
+++ TestappsortNumber/Num.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestappsortNumber
+{
+    class Num : IEnumerable<int>
+    {
+        public int[] testnums = new int[] { };
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            return ((IEnumerable<int>)testnums).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable<int>)testnums).GetEnumerator();
+        }
+    }
+}
