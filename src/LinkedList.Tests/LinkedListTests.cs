7804b91f0eb18ba1a29d87674a693ac8800eed37
Xigi <myrikor@gmail.com>
2020-02-27 00:06:02 +0600

    IEnumerabl staff

--- src/LinkedList.Tests/LinkedListTests.cs
+++ src/LinkedList.Tests/LinkedListTests.cs
@@ -9,54 +9,101 @@ namespace LinkedList.Tests
         static void Main(string[] args)
         {
             {
-                ICollection<int> sequence = new List<int>();
+                IEnumerable<int> sequence = new IntLinkedList(10);
 
-                if (sequence.Count == 0)
+                foreach (int element in sequence)
                 {
-                    Console.WriteLine("Nums 0 count PASS");
-                }
-                else
-                {
-                    Console.WriteLine("Nums 0 count FAILED");
-                }
-
-            }
-            {
-                ICollection<int> sequence = Enumerable.Range(0, 5).ToList();
-
-                if (sequence.Count == 5)
-                {
-                    Console.WriteLine("Nums fixed count PASS");
-                }
-                else
-                {
-                    Console.WriteLine("Nums fixed count FAILED");
+                    Console.WriteLine(element);
                 }
             }
             {
-                ICollection<int> sequence = Enumerable.Range(0, 5).ToList();
-                sequence.Remove(4);
-                if (sequence.Count == 4)
+                IEnumerable<int> sequence = new IntLinkedList();
+
+                if (sequence.Count() == 0)
                 {
-                    Console.WriteLine("Nums remove one PASS");
+                    Console.WriteLine("IntLinkedList 0 count PASS");
                 }
                 else
                 {
-                    Console.WriteLine("Nums remove one FAILED");
+                    Console.WriteLine("IntLinkedList 0 count FAILED");
                 }
             }
             {
-                ICollection<int> sequence = Enumerable.Range(0, 5).ToList();
-                sequence.Add(5);
-                if (sequence.Count == 6)
+                IEnumerable<int> sequence = new IntLinkedList(5);
+
+                if (sequence.Count() == 5)
                 {
-                    Console.WriteLine("Nums add one PASS");
+                    Console.WriteLine("IntLinkedList fixed count PASS");
                 }
                 else
                 {
-                    Console.WriteLine("Nums add one FAILED");
+                    Console.WriteLine("IntLinkedList fixed count FAILED");
                 }
             }
+            {
+                IEnumerable<int> sequence = new IntLinkedList(10).Take(5);
+
+                if (sequence.Last() == 4 && sequence.Count() == 5)
+                {
+                    Console.WriteLine("Nums take 5 PASS");
+                }
+                else
+                {
+                    Console.WriteLine("Nums take 5 FAILED");
+                }
+            }
+
+            //{
+            //    ICollection<int> sequence = new List<int>();
+
+            //    if (sequence.Count == 0)
+            //    {
+            //        Console.WriteLine("IntLinkedList 0 count PASS");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("IntLinkedList 0 count FAILED");
+            //    }
+
+            //}
+            //{
+            //    ICollection<int> sequence = Enumerable.Range(0, 5).ToList();
+
+            //    if (sequence.Count == 5)
+            //    {
+            //        Console.WriteLine("IntLinkedList fixed count PASS");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("IntLinkedList fixed count FAILED");
+            //    }
+            //}
+            //{
+            //    ICollection<int> sequence = Enumerable.Range(0, 5).ToList();
+            //    sequence.Remove(4);
+            //    if (sequence.Count == 4)
+            //    {
+            //        Console.WriteLine("IntLinkedList remove one PASS");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("IntLinkedList remove one FAILED");
+            //    }
+            //}
+            //{
+            //    ICollection<int> sequence = Enumerable.Range(0, 5).ToList();
+            //    sequence.Add(5);
+            //    if (sequence.Count == 6)
+            //    {
+            //        Console.WriteLine("IntLinkedList add one PASS");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("IntLinkedList add one FAILED");
+            //    }
+            //}
+
+
         }
     }
 }
---
+++ src/LinkedList/IntLinkedList.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList
+{
+    public class IntLinkedList : IEnumerable<int>
+    {
+
+        private Item _first;
+
+        public IntLinkedList(int size = 0)
+        {
+            if (size > 0)
+            {
+                Item tmp = new Item() { Value = 0 };
+                _first = tmp;
+
+                for (int i = 1; i < size; i++)
+                {
+                    tmp.Next = new Item() { Value = i };
+                    tmp = tmp.Next;
+                }
+            }
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            Item tmp = _first;
+            while (tmp != null)
+            {
+                yield return tmp.Value;
+                tmp = tmp.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private class Item
+        {
+            internal int Value;
+            internal Item Next;
+        }
+    }
+}
