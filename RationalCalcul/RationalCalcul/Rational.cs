da9949886c7c204d4a56d80a2118d3226100f174
Xigi <myrikor@gmail.com>
2020-03-05 21:29:03 +0600

    RationalCalcul

---
+++ RationalCalcul/RationalCalcul/Rational.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RationalCalcul
+{
+    public struct Rational
+    {
+        public Rational(int numerator = 0, int denominator = 1)
+        {
+            if (denominator < 1)
+            {
+                throw new ArgumentException("Denominator cannot be zero or negative");
+            }
+
+            Numerator = numerator;
+            Denominator = denominator;
+
+        }
+
+        public Rational(string s)
+        {
+            Rational result = Parse(s);
+
+            Numerator = result.Numerator;
+            Denominator = result.Denominator;
+
+        }
+        public static bool TryParse(string s, out Rational result)
+        {
+            result = new Rational(1, 1);
+            try
+            {
+                result = Parse(s);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+
+        }
+
+        public static Rational Parse(string s)
+        {
+            string[] values = s.Split("/");
+            if (values.Length != 2)
+            {
+                throw new ArgumentException($"Cannot parse to Rational - {s}");
+            }
+
+            if (!int.TryParse(values[0], out int numerator)
+              || !int.TryParse(values[1], out int denominator))
+            {
+                throw new ArgumentException($"Cannot parse to Rational - {s}");
+            }
+
+            return new Rational(numerator, denominator);
+        }
+
+        public int Numerator
+        {
+            get;
+        }
+
+        public int Denominator
+        {
+            get;
+        }
+
+        public static Rational operator +(Rational left, Rational right)
+        {
+            int a = left.Numerator;
+            int b = left.Denominator;
+
+            int c = right.Numerator;
+            int d = right.Denominator;
+
+            Rational result =  new Rational(a * d + c * b, b * d);
+            return CutFraction(result);
+
+        }
+
+        public static Rational operator -(Rational left, Rational right)
+        {
+            int a = left.Numerator;
+            int b = left.Denominator;
+
+            int c = right.Numerator;
+            int d = right.Denominator;
+
+            Rational result = new Rational(a * d - c * b, b * d);
+            return CutFraction(result);
+
+        }
+
+        public static Rational operator *(Rational left, Rational right)
+        {
+            int a = left.Numerator;
+            int b = left.Denominator;
+
+            int c = right.Numerator;
+            int d = right.Denominator;
+
+            Rational result = new Rational(a * c, b * d);
+            return CutFraction(result);
+        }
+
+        public static Rational operator /(Rational left, Rational right)
+        {
+            int a = left.Numerator;
+            int b = left.Denominator;
+
+            int c = right.Numerator;
+            int d = right.Denominator;
+
+            Rational result = new Rational(a * d, b * c);
+            return CutFraction(result);
+        }
+
+        public static bool operator == (Rational left, Rational right)
+        {
+            int a = left.Numerator;
+            int b = left.Denominator;
+
+            int c = right.Numerator;
+            int d = right.Denominator;
+
+            if (a == c & b == d)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public static bool operator != (Rational left, Rational right)
+        {
+            int a = left.Numerator;
+            int b = left.Denominator;
+
+            int c = right.Numerator;
+            int d = right.Denominator;
+
+            if (a != c || b != d)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override string ToString()
+        {
+            if (Numerator == 1 && Denominator == 1)
+            {
+                return $"1";
+            }
+            else
+            {
+                return $"{Numerator}/{Denominator}";
+            }
+            
+        }
+
+        private static Rational CutFraction(Rational p)
+        {
+            int NOD = GetNOD(p.Numerator, p.Denominator);
+            return new Rational(p.Numerator/ NOD, p.Denominator/ NOD);
+        }
+
+       private static int GetNOD(int val1, int val2)
+        {
+            while ((val1 != 0) && (val2 != 0))
+            {
+                if (val1 > val2)
+                    val1 -= val2;
+                else
+                    val2 -= val1;
+            }
+            return Math.Max(val1, val2);
+        }
+    }
+}
---
+++ RationalCalcul/Test/RationalTest.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+
+namespace RationalCalcul.Test
+{
+    class RationalTest
+    {
+        static void Main()
+        {
+            {
+                Rational first = new Rational(1, 2);
+                Rational second = new Rational(2, 3);
+                Rational result = first + second;
+                if (result == new Rational(7, 6))
+                {
+                    Console.WriteLine("Test summ PASS");
+                }
+                else
+                {
+                    Console.WriteLine("Test summ FAILED");
+                }
+            }
+
+            {
+                Rational first = new Rational(1, 2);
+                Rational second = new Rational(1, 3);
+                Rational result = first - second;
+                if (result == new Rational(1, 6))
+                {
+                    Console.WriteLine("Test minus PASS");
+                }
+                else
+                {
+                    Console.WriteLine("Test minus FAILED");
+                }
+            }
+
+            {
+                Rational first = new Rational(1, 2);
+                Rational second = new Rational(2, 3);
+                Rational result = first * second;
+                if (result == new Rational(1, 3))
+                {
+                    Console.WriteLine("Test multy PASS");
+                }
+                else
+                {
+                    Console.WriteLine("Test multy FAILED");
+                }
+            }
+
+            {
+                Rational first = new Rational(1, 2);
+                Rational second = new Rational(2, 3);
+                Rational result = first / second;
+                if (result == new Rational(3, 4))
+                {
+                    Console.WriteLine("Test division PASS");
+                }
+                else
+                {
+                    Console.WriteLine("Test division FAILED");
+                }
+            }
+
+            {
+                Rational first = new Rational(2, 4);
+                Rational second = new Rational(2, 4);
+                Rational result = first + second;
+                if (result == new Rational(1, 1))
+                {
+                    Console.WriteLine("Test cut metod PASS");
+                }
+                else
+                {
+                    Console.WriteLine("Test cut metod FAILED");
+                }
+            }
+
+            {
+                try
+                {
+                    Rational first = new Rational("asdfe");
+                    Console.WriteLine("Test Exception FAILED");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Test Exception PASS   " + ex.Message);
+                }
+            }
+        }
+    }
+}
