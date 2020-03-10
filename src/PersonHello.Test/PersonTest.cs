7f15f845b7f9e414ec9d262167d5aaaf9417688b
Xigi <myrikor@gmail.com>
2020-03-10 22:21:34 +0600

    ass new project Hello person

---
+++ src/PersonHello.Test/PersonTest.cs
@@ -0,0 +1,119 @@
+﻿using System;
+
+namespace PersonHello.Test
+{
+    class PersonTest
+    {
+        static void Main(string[] args)
+        {
+            {
+                Person Person = new Person("Piter", "Garms", "M", 66);
+                if(Person.Hello() == "Hello Piter you already enjoy your retirement")
+                {
+                    Console.WriteLine("Test age man PASS");
+                }
+                else
+                {
+                    Console.WriteLine("Test age man FAILED");
+                }
+            }
+
+            {
+                Person Person = new Person("Dasha", "Garms", "F", 66);
+                if (Person.Hello() == "Hello grandma Dasha")
+                {
+                    Console.WriteLine("Test Age female PASS");
+                }
+                else
+                {
+                    Console.WriteLine("Test Age female FAILED");
+                }
+            }
+
+            {
+                Person Person = new Person("Piter", "Garms", "M", 40);
+                if (Person.Hello() == "hi man how do you live in your 40s")
+                {
+                    Console.WriteLine("Test middle-age man PASS");
+                }
+                else
+                {
+                    Console.WriteLine("Test middle-age man FAILED");
+                }
+            }
+
+            {
+                Person Person = new Person("Dasha", "Garms", "F", 40);
+                if (Person.Hello() == "hello beauty I would never say that to you 40")
+                {
+                    Console.WriteLine("Test middle-age female PASS");
+                }
+                else
+                {
+                    Console.WriteLine("Test middle-age female FAILED");
+                }
+            }
+
+            {
+                Person Person = new Person("Dasha", "Garms", "F", 17);
+                if (Person.Hello() == "Hello girl, do you already know how to spend an owl's 18th birthday?")
+                {
+                    Console.WriteLine("Test birthday < 17 female PASS");
+                }
+                else
+                {
+                    Console.WriteLine("Test birthday < 17 female FAILED");
+                }
+            }
+
+            {
+                Person Person = new Person("Piter", "Garms", "M", 17);
+                if (Person.Hello() == "Hi, how are you going to the army? you are already 17 years old, which means that in 1 years you will be in the army")
+                {
+                    Console.WriteLine("Test birthday < 17 man PASS");
+                }
+                else
+                {
+                    Console.WriteLine("Test birthday < 17 man FAILED");
+                }
+            }
+
+            {
+
+                Person Person = new Person("Piter", "Garms", "M", 82);
+                if (Person.Hello() == "Hello! Mr. Piter, how did you manage to live to such an age")
+                {
+                    Console.WriteLine("Test age > 80 man PASS");
+                }
+                else
+                {
+                    Console.WriteLine("Test age > 80 man FAILED");
+                }
+            }
+
+            {
+                Person Person = new Person("Dasha", "Garms", "F", 82);
+                if (Person.Hello() == "Hello! Ms. Dasha, how did you manage to live to such an age")
+                {
+                    Console.WriteLine("Test age > 80 female PASS");
+                }
+                else
+                {
+                    Console.WriteLine("Test age > 80 female FAILED");
+                }
+
+                {
+                    try
+                    {
+                        new Person("Dasha", "Garms", "F", -1);
+                        Console.WriteLine("Test Exception FAILED");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Test Exception PASS   " + ex.Message);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/PersonHello/Person.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace PersonHello
+{
+    public class Person
+    {
+        //private DateTime Birthday
+        //{
+        //    get;
+        //}
+        private String Name
+        {
+            get;
+        }
+
+        private String LastName
+        {
+            get;
+        }
+
+        private String Sex
+        {
+            get;
+        }
+
+        private int Age
+        {
+            get;
+        }
+
+        public Person(String name, String lastname, String sex, int age)
+        {
+            if (age < 0)
+            {
+                throw new ArgumentException("age cannot be negative");
+            }
+
+            Name = name;
+            LastName = lastname;
+            Sex = sex;
+            Age = age;
+        }
+
+        public String Hello()
+        {
+
+            if (Age > 65 && Age < 80 && Sex == "M")
+            {
+                return $"Hello { Name } you already enjoy your retirement";
+            }
+
+            if (Age > 65 && Age < 80 && Sex == "F")
+            {
+                return $"Hello grandma { Name }";
+            }
+
+            if (Age < 65 && Age > 18 && Sex == "M")
+            {
+                return $"hi man how do you live in your {Age}s";
+            }
+
+            if (Age < 65 && Age > 18 && Sex == "F")
+            {
+                return $"hello beauty I would never say that to you {Age}";
+            }
+
+            if (Age < 18 && Sex == "F")
+            {
+                return $"Hello girl, do you already know how to spend an owl's 18th birthday?";
+            }
+
+            if (Age < 18 && Sex == "M")
+            {
+                return $"Hi, how are you going to the army? you are already {Age} years old, which means that in {18-Age} years you will be in the army";
+            }
+
+            if (Age > 80 && Sex == "M")
+            {
+                return $"Hello! Mr. { Name }, how did you manage to live to such an age";
+            }
+
+            if (Age > 80 && Sex == "F")
+            {
+                return $"Hello! Ms. { Name }, how did you manage to live to such an age";
+            }
+
+            return $"Hello { Name }";
+        }
+    }
+}
