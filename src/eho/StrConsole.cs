c295f5c2e09f6e121c5a4e66e92f441a12d7d032
DEUS <tragoms@gmail.com>
2020-03-17 11:04:51 +0600

    add eho project

---
+++ src/eho/StrConsole.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eho
+{
+    class StrConsole
+    {
+        private static string EhoMessage
+        {
+            get;
+        }
+
+        static StrConsole()
+        {
+            Console.Write("Ваше сообщение: ");
+            EhoMessage = Console.ReadLine();
+            Console.Write("Ввидет команду: ");
+            OperationModeSelection(Console.ReadLine());
+        }
+
+        private static void OperationModeSelection(string workmod)
+        {
+           
+            if (workmod == "-f" || workmod == "-F" )
+            {
+                Console.WriteLine( );
+                StrFile.FileRecord(EhoMessage);
+            }
+
+            else if (workmod == "-k" || workmod == "-K")
+            {
+                Console.WriteLine("\n" + EhoMessage + "\n");
+                Thread.Sleep(5000);
+            }
+
+            else if (workmod == "-h" || workmod == "-H")
+            {
+                ConsoleReference();
+            }
+
+            else
+            {
+                Console.Write("Вы ввели недопустимую команду. Для просмотра всех команд введите -h \n"+
+                    "Ввидет команду: ");
+                OperationModeSelection(Console.ReadLine());
+            }
+        }
+
+        private static void ConsoleReference()
+        {
+            Console.WriteLine("Список команд: ");
+            Console.Write(
+                "\n" +
+                "-k Вывод сообщения в консоль \n" +
+                "-f Вывод сообщения в фаил    \n" +
+                "-h Справка по команндам      \n" +
+                "Ввидет команду: ");
+           OperationModeSelection(Console.ReadLine());
+        }
+    }
+}
---
+++ src/eho/StrFile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+
+namespace eho
+{
+    class StrFile
+    {
+        static string  writePath = @"C:\Users\DEUS\Desktop\ehomassage.txt";
+
+        public static void FileRecord(string message)
+        {
+            try
+            {
+                using StreamWriter sw = new StreamWriter(writePath, false, System.Text.Encoding.Default);
+                sw.WriteLine(message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
+
